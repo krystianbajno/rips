e6a77783151549279953e291f4cfb55c337ae30e
pwn <krystian.bajno@pitradwar.com>
2025-04-23 13:01:28 +0200

    rips

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+use ipnetwork::IpNetwork;
+use std::env;
+use std::process;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 2 {
+        eprintln!("Usage: prips <subnet>");
+        process::exit(1);
+    }
+
+    let cidr = &args[1];
+    match cidr.parse::<IpNetwork>() {
+        Ok(network) => {
+            for ip in network.iter() {
+                println!("{}", ip);
+            }
+        }
+        Err(_) => {
+            eprintln!("Invalid CIDR format: {}", cidr);
+            process::exit(1);
+        }
+    }
+}
